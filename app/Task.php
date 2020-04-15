bcec16b242d83cb78f87b7d484849392256f2c93
may-tech <aehkloprsw@gmail.com>
2020-04-15 14:50:54 +0900

    [add] classを返す記述追加

--- app/Task.php
+++ app/Task.php
@@ -10,9 +10,9 @@ class Task extends Model
      * 状態定義
      */
     const STATUS = [
-        1 => [ 'label' => '未着手' ],
-        2 => [ 'label' => '着手中' ],
-        3 => [ 'label' => '完了' ],
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
     ];
 
     /**
@@ -29,6 +29,6 @@ class Task extends Model
             return '';
         }
 
-        return self::STATUS[$status]['label'];
+        return self::STATUS[$status]['class'];
     }
 }
