6c96ac6ffbf69cb13a4a574a31c1a764252bd0f9
may-tech <aehkloprsw@gmail.com>
2020-05-03 11:30:06 +0900

    [add] ページ認証

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+  //認証されていたら実行されるように！
+Route::group(['middleware' => 'auth'], function() {
+  Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-Route::post('/folders/create', 'FolderController@create');
+  Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+  
+  Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+  Route::post('/folders/create', 'FolderController@create');
 
-Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+  Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+  Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 
-//タスク編集機能
-Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
-
-Route::get('/', 'HomeController@index')->name('home');
+  //タスク編集機能
+  Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+  Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+});
 
 Auth::routes();
